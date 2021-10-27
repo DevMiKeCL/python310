5fb3030a41eaf5c62b969dfd43c4faee0378d422
Miguel <miguelperaltad@gmail.com>
2021-10-27 12:05:07 -0300

    prueba

---
+++ Seccion001/main.py
@@ -0,0 +1,2 @@
+print("hola mundo desde phyton")
+print("nueva version")
---
+++ Seccion002/main.py
@@ -0,0 +1,25 @@
+miVariable = "Hola desde python"
+print(miVariable)
+print(miVariable)
+print(miVariable)
+
+miVariable = 2
+print(miVariable)
+print()
+
+# es posible que la variable cambie de tipo solamenete asigando un nuevo valor
+
+x = 10
+y = 2
+z = x + y
+print(x)
+print(y)
+print(z)
+print(id(x))
+print(id(y))
+print(id(z))
+
+print(type(x))
+x = "hola"
+print(x)
+print(type(x))
